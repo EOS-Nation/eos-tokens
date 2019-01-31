c342c464ece50ed04aa600fe43ac279b8bc4555f
Denis <carriere.denis@gmail.com>
2019-01-31 18:46:59 -0500

    Check for precision mismatch

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
+import fs from "fs";
 import test from 'ava';
+import path from "path";
+import load from "load-json-file";
 import tokens from './';
 
 for (const token of tokens) {
-    const {account, symbol} = token;
+    const {account, symbol, precision} = token;
     const label = `[${account}::${symbol}]`;
 
     // Test for required fields
@@ -10,6 +13,22 @@ for (const token of tokens) {
     for (const field of required) {
         test(`${label} ${field}`, t => {
             if (token[field] === undefined) t.fail(`${label} ${field} is missing`)
+            t.pass()
+        })
+    }
+
+    // Validate against `get_currency_stats`
+    // must `download.js` first
+    const statsfilepath = path.join(__dirname, "get_currency_stats", account, symbol + ".json")
+
+    if (fs.existsSync(statsfilepath)) {
+        const stats = load.sync(statsfilepath)
+
+        test(`${label} get_currency_stats`, t => {
+            const [amount, sym] = stats[symbol].supply.split(' ');
+            let [major, minor] = amount.split('.')
+            if (!minor) minor = ""
+            if (minor.length !== precision) t.fail(`${label} precision is mismatched (${minor.length} !== ${precision})`)
             t.pass()
         })
     }
